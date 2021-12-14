bd9425e8b4bfcc1bed971506ae2b18209f186368
Verkunder <Verkunder614@gmail.com>
2021-12-14 10:40:46 +0300

    1

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 Vue.createApp ({
     data: () => ({
-        title: 'Я есть Грут'
-    })
+        title: 'Я есть Грут!',
+        myHTML: '<h1> Hello Vue 3 </h1>',
+        person: {
+            firstName: 'Kapustin',
+            lastName: 'Stepan',
+            age: 22
+        },
+        items: [1, 2]
+    }),
+    computed: {
+        eventItems(){
+            return this.items.filter(i => i % 2 === 0);
+        }
+    },
+    methods: {
+        addItem(){
+            this.items.unshift(this.$refs.myInput.value)
+            this.$refs.myInput.value = ''
+        },
+        remove (i) {
+            this.items.splice(i, 1)
+        },
+        log (item) {
+            console.log(item)
+        }
+    }
 
 }).mount ('#app')

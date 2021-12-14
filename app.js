caa8797794ddeb4c58c7f53ebb3b5b94ad1c3630
Verkunder <Verkunder614@gmail.com>
2021-12-14 17:13:23 +0300

    1

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-Vue.createApp ({
-    data: () => ({
-        title: 'Я есть Грут!',
-        myHTML: '<h1> Hello Vue 3 </h1>',
-        person: {
-            firstName: 'Kapustin',
-            lastName: 'Stepan',
-            age: 22
-        },
-        items: [1, 2]
-    }),
-    computed: {
-        eventItems(){
-            return this.items.filter(i => i % 2 === 0);
+const h = Vue.h
+
+const app = Vue.createApp ({
+    data() {
+        return {
+            title: 'Vue 3'
         }
     },
     methods: {
-        addItem(){
-            this.items.unshift(this.$refs.myInput.value)
-            this.$refs.myInput.value = ''
-        },
-        remove (i) {
-            this.items.splice(i, 1)
-        },
-        log (item) {
-            console.log(item)
+        changeTitle () {
+            console.log (this.title)
+            this.title = 'Vue 3.1'
+        }
+    },
+    render() {
+        return h ('div', {
+            class: 'card center'
+        },[
+            h('h1', {}, this.title),
+            h('button', {
+                class: 'btn',
+                onClick: this.changeTitle
+            }, 'Update')    
+    ] )
+    }
+})
+const app2 = Vue.createApp ({
+    data() {
+        return {
+            title: 'Vue 3.2'
         }
     }
-
-}).mount ('#app')
+})
+app.mount('#app')
+app2.mount('#app2')

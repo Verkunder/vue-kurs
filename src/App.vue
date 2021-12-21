50c2cf16800da554bf35aa824ac6357032a00b1f
Verkunder <Verkunder614@gmail.com>
2021-12-21 15:18:40 +0300

    change block-7

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,23 @@
   <div class="container">
     <form class="card" @submit.prevent="submitHandler">
       <h1>Анкета на Vue разработчика!</h1>
-      <div class="form-control" :class="{invalid: errors.name}">
-        <label for="name">Как тебя зовут?</label>
-        <input type="text"
-               id="name"
-               placeholder="Введи имя"
-               v-model.trim="name"
-        >
-        <small v-if="errors.name != null"> {{errors.name}} </small>
-      </div>
+      <!--      <div class="form-control" :class="{invalid: errors.name}">-->
+      <!--        <label for="name">Как тебя зовут?</label>-->
+      <!--        <input-->
+      <!--            type="text"-->
+      <!--            id="name"-->
+      <!--            placeholder="Введи имя"-->
+      <!--            v-model.trim="name"-->
+      <!--        >-->
+      <!--        <small v-if="errors.name">{{ errors.name }}</small>-->
+      <!--      </div>-->
+
+      <app-input
+          placeholder="Введи имя"
+          :error="errors.name"
+          label="Как тебя зовут?"
+          v-model:value="name"
+      ></app-input>
 
       <div class="form-control">
         <label for="age">Выбери возраст</label>
@@ -35,100 +43,91 @@
       <div class="form-checkbox">
         <span class="label">Готов к переезду в Токио?</span>
         <div class="checkbox">
-          <label><input v-model="relocation" type="radio" name="trip" value="yes" /> Да</label>
+          <label><input type="radio" v-model="relocate" name="trip" value="yes" /> Да</label>
         </div>
 
         <div class="checkbox">
-          <label><input v-model="relocation" type="radio" name="trip" value="no"/> Нет</label>
+          <label><input type="radio" v-model="relocate" name="trip" value="no" /> Нет</label>
         </div>
       </div>
 
       <div class="form-checkbox">
         <span class="label">Что знаешь во Vue?</span>
         <div class="checkbox">
-          <label><input type="checkbox" v-model="scill" name="skill" value="vuex"/> Vuex</label>
+          <label><input type="checkbox" v-model="skills" name="skills" value="vuex" /> Vuex</label>
         </div>
         <div class="checkbox">
-          <label><input type="checkbox" v-model="scill" name="skill" value="cli"/> Vue CLI</label>
+          <label><input type="checkbox" v-model="skills" name="skills" value="cli" /> Vue CLI</label>
         </div>
         <div class="checkbox">
-          <label><input type="checkbox" v-model="scill" name="skill" value="route"/> Vue Router</label>
+          <label><input type="checkbox" v-model="skills" name="skills" value="router" /> Vue Router</label>
         </div>
       </div>
 
-      <div class="form-checkbox" :class="{invalid: errors.aqree}">
+      <div class="form-checkbox">
         <span class="label">Правила нашей компании</span>
-        <div class="checkbox" >
-          <label> <input type="checkbox" v-model="aqree"/> С правилами согласен(а) </label>
+        <div class="checkbox">
+          <label><input type="checkbox" v-model="agree" /> С правилами согласен</label>
         </div>
-        <small v-if="errors.aqree != null"> {{errors.aqree}} </small>
       </div>
+
       <button type="submit" class="btn primary">Отправить</button>
     </form>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
+import AppInput from './AppInput'
+export default {
+  data() {
+    return {
       name: '',
       age: 23,
-      city: 'nsk',
-      relocation: null,
-      scill: [],
-      aqree: false,
+      city: 'spb',
+      relocate: 'yes',
+      skills: [],
+      agree: false,
       errors: {
-        name: null,
-        aqree: null
+        name: null
       }
+    }
+  },
+  components: {AppInput},
+  methods: {
+    formIsValid() {
+      let isValid = true
+
+      if (this.name.length === 0) {
+        this.errors.name = 'Имя не может быть пустым'
+        isValid = false
+      } else {
+        this.errors.name = null
       }
+
+      return isValid
     },
-    methods: {
-      formIsValid() {
-        let isValid = true
-        if (this.name.length === 0) {
-          this.errors.name = 'Это поле обязательное ⚠'
-          isValid = false
-        } else {
-          this.errors.name = null
-        }
-        if (this.aqree === false){
-          this.errors.aqree = 'Вы не согласились с правилами компании ⚠'
-          isValid = false
-        } else {
-          this.errors.aqree = null
-        }
-        return isValid
-      },
-      submitHandler() {
-        if (this.formIsValid()) {
-          console.group('Form data')
-          console.log('Name:', this.name)
-          console.log('Age:', this.age)
-          console.log('city:', this.city)
-          console.log('To Tokyo:', this.relocation)
-          console.log('Scill:', this.scill)
-          console.log('Aqree:', this.aqree)
-          console.groupEnd()
-        }
+    submitHandler() {
+      if (this.formIsValid()) {
+        console.group('Form Data')
+        console.log('Name:', this.name)
+        console.log('Age:', this.age)
+        console.log('City:', this.city)
+        console.log('To Tokyo:', this.relocate)
+        console.log('Skills:', this.skills)
+        console.log('Agree:', this.agree)
+        console.groupEnd()
       }
     }
   }
+}
 </script>
 
 <style>
 .form-control small {
   color: #e53935;
-  padding-left: 25px;
-  font-size: 18px;
 }
+
 .form-control.invalid input {
-border-color: #e53935;
-}
-.form-checkbox small {
-  color: #e53935;
-  padding-left: 25px;
-  font-size: 18px;
+  border-color: #e53935;
 }
 </style>

2f8c7f63b871afbfae9d291b0a75d4628b668279
Verkunder <Verkunder614@gmail.com>
2021-12-23 09:24:02 +0300

    change block---9

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>
+  <div class="container">
+      <div class="card">
+        <h2>No women no cry</h2>
 
+    <div class="form-control">
+      <label for="inp"> Активный по умолчанию</label>
+      <input v-focus type="text" id="inp">
+    </div>
+      </div>
   </div>
 </template>
 
 <script>
-export default {
+import focusDirectiv from "./focusDirectiv";
 
+export default {
+  directives: {
+    focus: focusDirectiv
+  }
 }
 </script>
 

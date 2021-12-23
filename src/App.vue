18e203bddf6ea16ecfc66cf39179bd5755a8bb08
Verkunder <Verkunder614@gmail.com>
2021-12-23 10:38:22 +0300

    change block---9

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
 <template>
   <div class="container">
       <div class="card">
-        <h2>No women no cry</h2>
+        <h2 v-color="myColor">No women no cry</h2>
 
     <div class="form-control">
       <label for="inp"> Активный по умолчанию</label>
       <input v-focus type="text" id="inp">
     </div>
+        <button class="btn" @click="myColor = 'darkblue'">Поменять цвет заголовка</button>
       </div>
   </div>
 </template>
 
 <script>
 import focusDirectiv from "./focusDirectiv";
+import colorDirective from "./ColorDirective";
 
 export default {
   directives: {
-    focus: focusDirectiv
+    focus: focusDirectiv,
+    color: colorDirective
+  },
+  data() {
+    return {
+      myColor: 'darkred'
+    }
   }
 }
 </script>
---
+++ src/ColorDirective.js
@@ -0,0 +1,12 @@
+export default {
+    mounted (el, binding) {
+        console.log(el)
+        console.log(binding)
+        el.style.color = binding.value
+    },
+    updated (el, binding) {
+        console.log(el)
+        console.log(binding)
+        el.style.color = binding.value
+    }
+}

cf1fa98fd9366db23e45178bdec7b2d055c9c962
Verkunder <Verkunder614@gmail.com>
2022-01-12 10:30:18 +0300

    add Navigation  -MainLayout  -AuthLayout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
-  <router-view/>
+
+  <component :is="layout + '-layout'" v-if="layout" />
 </template>
 
-<style>
+<script>
+import {computed} from 'vue';
+import {useRoute} from 'vue-router'
+import AuthLayout from "./layout/AuthLayout";
+import MainLayout from "./layout/MainLayout";
+export default {
+  setup() {
+    const route = useRoute()
+
+    return {
+      layout: computed(() => route.meta.layout)
+    }
+  },
+  components: {AuthLayout, MainLayout}
+}
+</script>
+
+<style scoped>
 
 </style>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar">
+    <h3>Online Bank</h3>
+    <ul class="navbar-menu">
+      <li>
+        <router-link to="/">Applications</router-link>
+      </li>
+      <li>
+        <router-link to="/">Help</router-link>
+      </li>
+      <li>
+        <a href="#">Message</a>
+      </li>
+      <li>
+        <a href="#">Exit</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
